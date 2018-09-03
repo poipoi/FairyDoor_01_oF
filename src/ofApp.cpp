7e28db68a4700af3f14c4bfb706f7b92a9fd820c
poipoi <poipoi.11110@gmail.com>
2018-09-03 09:55:25 +0900

    GUI Added

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,26 +1,80 @@
 #include "ofApp.h"
 
+#define BUTTON_SIZE     (200)
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 	sender.setup("192.168.0.18", 12345);
 
 	//serial.setup("COM6", 115200);
+    
+    bellBtn.setup(BUTTON_SIZE * 0, 0, BUTTON_SIZE, BUTTON_SIZE, "bell_on.png", "bell_off.png");
+    knockBtn.setup(BUTTON_SIZE * 1, 0, BUTTON_SIZE, BUTTON_SIZE, "knock_on.png", "knock_off.png");
+    lampBtn.setup(BUTTON_SIZE * 2, 0, BUTTON_SIZE, BUTTON_SIZE, "lamp_on.png", "lamp_off.png");
+    brightBtn.setup(BUTTON_SIZE * 3, 0, BUTTON_SIZE, BUTTON_SIZE, "bright_on.png", "bright_off.png");
+
+    
+    bellAction.durTime = 0.1;
+    bellAction.startFunc = [&]() {
+        cout << "Bell ON" << endl;
+        setBell(true);
+    };
+    bellAction.finishFunc = [&]() {
+        cout << "Bell OFF" << endl;
+        setBell(false);
+        bellBtn.turnOFF();
+    };
+    
+    knockAction.durTime = 0.1;
+    knockAction.startFunc = [&]() {
+        cout << "Knock ON" << endl;
+        setKnock(true);
+    };
+    knockAction.finishFunc = [&]() {
+        cout << "Knock OFF" << endl;
+        setKnock(false);
+        knockBtn.turnOFF();
+    };
+    
+    lampOnAction.durTime = 1.0;
+    lampOnAction.durFunc = [&](float ratio) {
+        cout << "Lamp ON:" << ratio << endl;
+        setLamp(lampR, lampG, lampB, ratio);
+    };
+
+    lampOffAction.durTime = 1.0;
+    lampOffAction.durFunc = [&](float ratio) {
+        cout << "Lamp OFF:" << ratio << endl;
+        setLamp(lampR, lampG, lampB, 1.0 - ratio);
+    };
+
+    brightOnAction.durTime = 1.0;
+    brightOnAction.durFunc = [&](float ratio) {
+        cout << "Bright ON:" << ratio << endl;
+        setBright(brightR, brightG, brightB, ratio);
+    };
+    
+    brightOffAction.durTime = 1.0;
+    brightOffAction.durFunc = [&](float ratio) {
+        cout << "Bright OFF:" << ratio << endl;
+        setBright(brightR, brightG, brightB, 1.0 - ratio);
+    };
 
 	gui.setup();
-
-	gui.add(brightR.setup("BRIGHT_R", 0, 0, 255));
-	brightR.addListener(this, &ofApp::onBrightChanged);
-	gui.add(brightG.setup("BRIGHT_G", 0, 0, 255));
-	brightG.addListener(this, &ofApp::onBrightChanged);
-	gui.add(brightB.setup("BRIGHT_B", 0, 0, 255));
-	brightB.addListener(this, &ofApp::onBrightChanged);
-
+    
 	gui.add(lampR.setup("LAMP_R", 0, 0, 255));
 	lampR.addListener(this, &ofApp::onLampChanged);
-	gui.add(lampG.setup("LAMP_G", 0, 0, 255));
+	gui.add(lampG.setup("LAMP_G", 100, 0, 255));
 	lampG.addListener(this, &ofApp::onLampChanged);
 	gui.add(lampB.setup("LAMP_B", 0, 0, 255));
 	lampB.addListener(this, &ofApp::onLampChanged);
+
+    gui.add(brightR.setup("BRIGHT_R", 0, 0, 255));
+    brightR.addListener(this, &ofApp::onBrightChanged);
+    gui.add(brightG.setup("BRIGHT_G", 255, 0, 255));
+    brightG.addListener(this, &ofApp::onBrightChanged);
+    gui.add(brightB.setup("BRIGHT_B", 0, 0, 255));
+    brightB.addListener(this, &ofApp::onBrightChanged);
 }
 
 //--------------------------------------------------------------
@@ -31,77 +85,67 @@ void ofApp::update(){
 		cout << c;
 	}
 	*/
+    
+    bellAction.update();
+    knockAction.update();
+    lampOnAction.update();
+    lampOffAction.update();
+    brightOnAction.update();
+    brightOffAction.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	gui.draw();
+    bellBtn.draw();
+    knockBtn.draw();
+    lampBtn.draw();
+    brightBtn.draw();
+    
+    if (guiIsVisible) gui.draw();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 	switch (key) {
-	case '1':
-		{
-			ofxOscMessage msg;
-			msg.setAddress("/fairydoor/sole");
-			msg.addInt32Arg(0);
-			msg.addInt32Arg(1);
-			sender.sendMessage(msg, false);
+        case '1':
+            bellAction.start();
+            bellBtn.turnON();
+            break;
 
-			//serial.writeBytes("001\n", 4);
-
-			cout << "Send" << endl;
-		}
-	break;
-
-	case '2':
-		{
-			ofxOscMessage msg;
-			msg.setAddress("/fairydoor/sole");
-			msg.addInt32Arg(1);
-			msg.addInt32Arg(1);
-			sender.sendMessage(msg, false);
-
-			//serial.writeBytes("011\n", 4);
-
-			cout << "Send" << endl;
-			break;
-		}
+        case '2':
+            knockAction.start();
+            knockBtn.turnON();
+            break;
+        
+        case '3':
+            if (!lampBtn.isON) {
+                lampOnAction.start();
+                lampBtn.turnON();
+            } else {
+                lampOffAction.start();
+                lampBtn.turnOFF();
+            }
+            break;
+            
+        case '4':
+            if (!brightBtn.isON) {
+                brightOnAction.start();
+                brightBtn.turnON();
+            } else {
+                brightOffAction.start();
+                brightBtn.turnOFF();
+            }
+            break;
+            
+        case 'd':
+            guiIsVisible = !guiIsVisible;
+            break;
 	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-	switch (key) {
-	case '1':
-		{
-			ofxOscMessage msg;
-			msg.setAddress("/fairydoor/sole");
-			msg.addInt32Arg(0);
-			msg.addInt32Arg(0);
-			sender.sendMessage(msg, false);
 
-			//serial.writeBytes("000\n", 4);
-
-			cout << "Send" << endl;
-		}
-		break;
-
-	case '2':
-		{
-			ofxOscMessage msg;
-			msg.setAddress("/fairydoor/sole");
-			msg.addInt32Arg(1);
-			msg.addInt32Arg(0);
-			sender.sendMessage(msg, false);
-
-			//serial.writeBytes("010\n", 4);
-
-			cout << "Send" << endl;
-		}
-		break;
-	}
 }
 
 //--------------------------------------------------------------
@@ -116,7 +160,10 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    bellBtn.onMousePressed(x, y, [&]() { bellAction.start(); });
+    knockBtn.onMousePressed(x, y, [&]() { knockAction.start(); });
+    lampBtn.onMousePressed(x, y, [&]() { lampOnAction.start(); }, [&]() { lampOffAction.start(); });
+    brightBtn.onMousePressed(x, y, [&]() { brightOnAction.start(); }, [&]() { brightOffAction.start(); });
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -23,27 +23,49 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+        void setBell(bool isON) {
+            ofxOscMessage msg;
+            msg.setAddress("/fairydoor/sole");
+            msg.addInt32Arg(0);
+            msg.addInt32Arg(isON ? 1 : 0);
+            sender.sendMessage(msg, false);
+        }
+    
+        void setKnock(bool isON) {
+            ofxOscMessage msg;
+            msg.setAddress("/fairydoor/sole");
+            msg.addInt32Arg(1);
+            msg.addInt32Arg(isON ? 1 : 0);
+            sender.sendMessage(msg, false);
+        }
+    
+        void setLamp(int r, int g, int b, float ratio) {
+            ofxOscMessage msg;
+            msg.setAddress("/fairydoor/lamp");
+            msg.addInt32Arg(r * ratio);
+            msg.addInt32Arg(g * ratio);
+            msg.addInt32Arg(b * ratio);
+            sender.sendMessage(msg, false);
+        }
 
+        void setBright(int r, int g, int b, float ratio) {
+            ofxOscMessage msg;
+            msg.setAddress("/fairydoor/bright");
+            msg.addInt32Arg(r * ratio);
+            msg.addInt32Arg(g * ratio);
+            msg.addInt32Arg(b * ratio);
+            sender.sendMessage(msg, false);
+        }
+    
 		void onBrightChanged(int& val) {
-			cout << "Bright:" << brightR << "," << brightG << "," << brightB << endl;
-
-			ofxOscMessage msg;
-			msg.setAddress("/fairydoor/bright");
-			msg.addInt32Arg(brightR);
-			msg.addInt32Arg(brightG);
-			msg.addInt32Arg(brightB);
-			sender.sendMessage(msg, false);
+            if (!brightBtn.isON) return;
+            setBright(brightR, brightG, brightB, 1.0);
 		}
 
 		void onLampChanged(int& val) {
-			cout << "Lamp:" << lampR << "," << lampG << "," << lampB << endl;
-
-			ofxOscMessage msg;
-			msg.setAddress("/fairydoor/lamp");
-			msg.addInt32Arg(lampR);
-			msg.addInt32Arg(lampG);
-			msg.addInt32Arg(lampB);
-			sender.sendMessage(msg, false);
+            if (!lampBtn.isON) return;            
+            setLamp(lampR, lampG, lampB, 1.0);
 		}
 
 private:
@@ -57,4 +79,89 @@ private:
 	ofxIntSlider lampR;
 	ofxIntSlider lampG;
 	ofxIntSlider lampB;
+    bool guiIsVisible = false;
+    
+    class OriginalButton {
+    public:
+        ofImage onImg;
+        ofImage offImg;
+        ofRectangle rect;
+        bool isON = false;
+        
+        void setup(float x, float y, float w, float h, string onImgPath, string offImgPath) {
+            rect.set(x, y, w, h);
+            onImg.load(onImgPath);
+            offImg.load(offImgPath);
+        }
+        
+        void turnON() {
+            isON = true;
+        }
+        
+        void turnOFF() {
+            isON = false;
+        }
+        
+        void onMousePressed(float x, float y, function<void(void)> onCallback = nullptr, function<void(void)> offCallback = nullptr) {
+            if (!rect.inside(x, y)) return;
+            
+            isON = !isON;
+            if (isON) {
+                if (onCallback) onCallback();
+            } else {
+                if (offCallback) offCallback();
+            }
+        }
+        
+        void draw() {
+            if (isON) {
+                onImg.draw(rect);
+            } else {
+                offImg.draw(rect);
+            }
+        }
+    };
+    
+    OriginalButton bellBtn;
+    OriginalButton knockBtn;
+    OriginalButton lampBtn;
+    OriginalButton brightBtn;
+
+    class OneAction {
+    public:
+        float durTime = 0;
+        function<void()> startFunc = nullptr;
+        function<void()> finishFunc = nullptr;
+        function<void(float)> durFunc = nullptr;
+        float onTime = 0;
+        bool isON = false;
+        
+        void start() {
+            onTime = ofGetElapsedTimef();
+            isON = true;
+            if (startFunc) startFunc();
+            if (durFunc) durFunc(0.0);
+        }
+        
+        void update() {
+            if (!isON) return;
+            
+            float ratio = ofMap(ofGetElapsedTimef(), onTime, onTime + durTime, 0.0, 1.0, false);
+            if (ratio < 1.0) {
+                if (durFunc) durFunc(ratio);
+            } else {
+                if (durFunc) durFunc(1.0);
+                if (finishFunc) finishFunc();
+                isON = false;
+            }
+        }
+    };
+    
+    OneAction bellAction;
+    OneAction knockAction;
+    OneAction lampOnAction;
+    OneAction lampOffAction;
+    OneAction brightOnAction;
+    OneAction brightOffAction;
 };
+
